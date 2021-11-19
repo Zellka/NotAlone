d9ab7c55de0e98341efb9484b76c79285c4a8c6b
sonya <sonp2401@gmail.com>
2021-11-20 01:02:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notalone/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.notalone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.notalone.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_menu)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.services_fragment,
+                R.id.bot_fragment,
+                R.id.chair_fragment,
+                R.id.friend_fragment,
+                R.id.profile_fragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
